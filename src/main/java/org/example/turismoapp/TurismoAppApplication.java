f672866d6cc2ecab379c64e4d938ec87b13f2759
pduran24 <pabloduranp5@gmail.com>
2026-02-01 11:03:41 +0100

    primer commit

---
+++ src/main/java/org/example/turismoapp/TurismoAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.turismoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurismoAppApplication.class, args);
+    }
+
+}
